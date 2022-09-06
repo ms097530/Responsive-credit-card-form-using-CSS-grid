1dfdc8148a1e1d64bb45d3bc4d41b79da164713f
Michael Schultz <mikewschultz9029@gmail.com>
2022-09-05 23:04:07 -0400

    added basic transition on form submission, error messages for invalid inputs, and cleaned up card number input formatting

--- scripts.js
+++ scripts.js
@@ -1,22 +1,31 @@
-// function addSpace(e)
-// {
-//     console.log(e);
-//     if (e.value.length % 4 === 0)
-//         e.value += ' ';
-// }
-
 const cardNumberInput = document.querySelector('#card-number');
 cardNumberInput.addEventListener('input', (e) =>
 {
-    if ([4, 9, 14].includes(e.target.value.length) && e.data !== null)
+
+    let removedSpaces = e.target.value.replaceAll(' ', '')
+    let removedSpacesArr = removedSpaces.split('')
+    let noSpacesLength = removedSpacesArr.length;
+    // makes sure space isn't added as the last character of the string so single deletion doesn't get stuck on space
+    let start = noSpacesLength % 4 === 0 ? noSpacesLength - 4 : noSpacesLength - noSpacesLength % 4;
+    // add spaces where necessary
+    for (let i = start; i >= 4; i -= 4)
     {
-        e.target.value += ' ';
+        removedSpacesArr.splice(i, 0, ' ');
     }
+    // seems setting e.target.value directly bypasses the element's maxlength, so this check makes sure the formatted length is not greater than maxlength
+    if (removedSpacesArr.length > 19)
+        removedSpacesArr.length = 19;
+    e.target.value = removedSpacesArr.join('');
 })
 
-const onlyNumbersInputs = document.querySelectorAll('.numbers-only');
-// console.log(onlyNumbersInputs);
-onlyNumbersInputs.forEach(() => addEventListener('input', (e) =>
+const submissionBtn = document.querySelector('#form-submit');
+submissionBtn.addEventListener('click', (e) =>
 {
+    e.preventDefault();
+    console.log(e);
+    const cardForm = document.querySelector('#card-form');
+    const confirmation = document.querySelector('#confirmation');
+    cardForm.classList.add('display-none');
+    confirmation.classList.remove('display-none');
 
-}))
+})
